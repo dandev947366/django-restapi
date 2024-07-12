cb25194e388cedbae8ae10c2949930212cf12a5f
danofoundation <dan.dev947366@gmail.com>
2024-07-12 18:31:10 +0300

    done

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -1,39 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-class App extends React.Component {
-  state = {
-    movies: []
-  };
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  componentDidMount() {
-    this.getMovies();
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/movieapi/");
+        setMovies(response.data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getMovies();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
-  getMovies() {
-    axios
-      .get("http://127.0.0.1:8000/movieapi/")
-      .then((res) => {
-        this.setState({ movies: res.data });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+
+  if (error) {
+    return <div>Error: {error}</div>;
   }
-  render() {
-    return (
-      <div className="App">
-        {this.state.movies.map((movie) => (
-          <div key={movie.id}>
-            <h1>{movie.name}</h1>
-            <img src={movie.image} alt={movie.name} />
-            <p>{movie.description}</p>
-            <p>Rating: {movie.rating}</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+
+  return (
+    <div className="App">
+      {movies.map((movie) => (
+        <div key={movie.id} className="movie-card">
+          <h1>{movie.name}</h1>
+          <img src={movie.image} alt={movie.name} />
+          <p>{movie.description}</p>
+          <p>Rating: {movie.rating}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
