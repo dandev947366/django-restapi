969cee5e0726e72e119ffec382cf6023ad55c98c
danofoundation <dan.dev947366@gmail.com>
2024-07-12 09:09:23 +0300

    Update views

--- mysite/movieapi/urls.py
+++ mysite/movieapi/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import MovieAPIView
+from .views import MovieAPIView, MovieDetail
 
 urlpatterns = [
     path("", MovieAPIView.as_view(), name="movie-list"),
+    path("<int:pk>/", MovieDetail.as_view(), name="movie-detail"),
 ]
--- mysite/movieapi/views.py
+++ mysite/movieapi/views.py
@@ -5,6 +5,11 @@ from rest_framework import generics
 from .serializers import MovieSerializer
 
 
-class MovieAPIView(generics.ListAPIView):
+class MovieAPIView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
